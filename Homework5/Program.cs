d60d67463dc7d2fa4a4a7578e6a4be69a1a70199
Katlintthk <pajukatlin@gmail.com>
2019-11-06 20:06:25 +0200

    homework

---
+++ Homework5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //vanus sekundites 
+            string userInput;
+            Console.WriteLine("Hello! Enter your birth date(yyyy/mm/dd): ");
+            userInput = Console.ReadLine();
+
+            string[] userData = userInput.Split('/');
+            int year = int.Parse(userData[0]);
+            int month = int.Parse(userData[1]);
+            int day = int.Parse(userData[2]);
+
+            var userBirthDay = new DateTime(year, month, day);
+            var now = DateTime.Now;
+            var seconds = (now - userBirthDay).TotalSeconds;
+            Console.WriteLine($"You were born {seconds} seconds ago.");
+
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
